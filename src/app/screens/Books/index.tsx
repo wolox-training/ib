44769dcf66dd4d13dbbf274803442c16fb02814b
Ivo Biaus <ivo.biaus@wolox.com.ar>
2020-04-16 15:45:04 -0300

    refactor

--- src/app/screens/Books/index.tsx
+++ src/app/screens/Books/index.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import {View, FlatList} from 'react-native';
 import BookCard from '../../components/BookCard';
 import styles from './styles';
-import {BookData} from '../../interfaces/book';
 import Data from '../../../../assets/Books.json'
 
 const Books = ({navigation}: any) => {
   const renderSeparator = () => <View style={styles.bookCardSeparator} />;
   const handleClick = () => navigation.navigate('Book Details');
-  const renderItem = (book: BookData) => <BookCard book={book} onClick={handleClick} />;
+  const renderItem = ({item}: any) =><BookCard book={item} onClick={handleClick} />;
+  const setId = (item: { id: string; }) => item.id;
 
   return (
     <View style={styles.mainContainer}>
       <FlatList
         data={Data}
-        renderItem={({item}) => renderItem(item)}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => renderSeparator()}
+        renderItem={renderItem}
+        keyExtractor={setId}
+        ItemSeparatorComponent={renderSeparator}
       />
     </View>
   );
