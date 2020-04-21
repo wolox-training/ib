fcbc366b4b3950e5ab5aaf7dada412c982758965
Ivo Biaus <ivo.biaus@wolox.com.ar>
2020-04-21 12:15:38 -0300

    the customBackButton now has functionality

--- src/app/components/AppNavigator/components/CustomBackButton/index.tsx
+++ src/app/components/AppNavigator/components/CustomBackButton/index.tsx
@@ -1,21 +1,19 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Image, View} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {Image, View, TouchableHighlight} from 'react-native';
 import styles from './styles';
 import backImg from './assets/ic_back.png';
 import Spacer from 'src/app/components/Spacer';
+import { StackHeaderLeftButtonProps } from '@react-navigation/stack';
 
-
-const Stack = createStackNavigator();
-
-function CustomBackButton() {
-
+function CustomBackButton(props: StackHeaderLeftButtonProps) {
   return (
-    <View style={styles.container}>
-      <Spacer width={15}/>
-      <Image source={backImg} style={styles.image}/>
-    </View>
+    <TouchableHighlight onPress={props.onPress}>
+      <View style={styles.container}>
+        <Spacer width={15} />
+        <Image source={backImg} style={styles.image} />
+      </View>
+    </TouchableHighlight>
   );
 }
 
