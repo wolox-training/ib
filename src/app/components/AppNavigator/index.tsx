24cbe45b4b1731ed9da6c710d70f1452578bd540
Ivo Biaus <ivo.biaus@wolox.com.ar>
2020-04-21 11:35:19 -0300

    added a custom header to the BookDetails screen

--- src/app/components/AppNavigator/index.tsx
+++ src/app/components/AppNavigator/index.tsx
@@ -2,21 +2,35 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
 import Routes from 'src/constants/routes';
 import Books from 'src/app/screens/Books';
 import BookDetails from 'src/app/screens/BookDetails';
+import CustomHeader from './components/CustomHeader';
+import CustomBackButton from './components/CustomBackButton';
+import { Platform } from 'react-native';
+import { white } from 'src/constants/colors';
 
 const Stack = createStackNavigator();
-
 function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={Routes.Books}>
         <Stack.Screen name={Routes.Books} component={Books} />
-        <Stack.Screen name={Routes.Details} component={BookDetails} />
+        <Stack.Screen
+          name={Routes.Details}
+          component={BookDetails}
+          
+          options={{
+            headerBackground: () => <CustomHeader />,
+            headerLeft: () => <CustomBackButton />,
+            headerTintColor: white,
+            headerTitleAlign: Platform.OS == 'ios' ? 'center' : 'left',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default AppNavigator;
