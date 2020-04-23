c4f052e0a9b783fe5ed2b949d6869d40ae8d251c
Ivo Biaus <ivo.biaus@wolox.com.ar>
2020-04-23 11:42:03 -0300

    deleted mapStateToProps, refactored Stack options

--- src/app/screens/Books/index.tsx
+++ src/app/screens/Books/index.tsx
@@ -3,7 +3,6 @@ import {View, FlatList} from 'react-native';
 
 import BookCard from '../../components/BookCard';
 import styles from './styles';
-<<<<<<< HEAD
 import {useSelector} from 'react-redux';
 
 function Books({navigation}: any) {
@@ -14,17 +13,6 @@ function Books({navigation}: any) {
   const setId = (item: {id: string}) => item.id;
 
   const library = useSelector((state: any) => state.book);
-=======
-import Data from '../../../../assets/Books.json';
-import {connect} from 'react-redux';
-
-function Books({navigation}: any) {
-  const renderSeparator = () => <View style={styles.bookCardSeparator} />;
-  const renderItem = ({item}: any) => (
-    <BookCard book={item} navigation={navigation} />
-  );
-  const setId = (item: {id: string}) => item.id;
->>>>>>> Added a TabBar, refactored the Stack navigation
 
   return (
     <View style={styles.mainContainer}>
@@ -38,8 +26,4 @@ function Books({navigation}: any) {
   );
 }
 
-const mapStateToProps = (state: any) => ({
-  library: state.books
-});
-
-export default connect(mapStateToProps)(Books);
+export default Books;
