50d01e095588945d742b4fc6f7d3d5c7c91d15e8
Ivo Biaus <ivo.biaus@wolox.com.ar>
2020-04-28 08:35:28 -0300

    Added actions types

--- src/redux/Rentals/actions.js
+++ src/redux/Rentals/actions.js
@@ -1,9 +1,11 @@
+export const actions = {ADD_RENTAL: "ADD_RENTAL", REMOVE_RENTAL: "REMOVE_RENTAL"};
+
 export const addRental = book => ({
-    type: "ADD_RENTAL",
+    type: actions.ADD_RENTAL,
     book
 })
 
 export const removeRental = book => ({
-    type: "REMOVE_RENTAL",
+    type: actions.REMOVE_RENTAL,
     book
 })
